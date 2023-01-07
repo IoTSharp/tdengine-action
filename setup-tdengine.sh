f43b9640615cb4f5ef7ac7e5637a0eff762b9e9f
MysticBoy <mysticboy@live.com>
2023-01-07 19:44:54 +0800

    taosadapter

--- setup-tdengine.sh
+++ setup-tdengine.sh
@@ -2,6 +2,7 @@
 if [ "$TDENGINE_START" = "true" ]
 then
     sudo systemctl start taosd
+    sudo systemctl start taosadapter 
     export TDENGINE_REST=$(hostname):6041/rest/sql
     until  curl -L -H "Authorization: Basic cm9vdDp0YW9zZGF0YQ=="   -d "SELECT SERVER_STATUS();"  $TDENGINE_REST ; do sleep 1 &&  systemctl status taosd; done
 fi
