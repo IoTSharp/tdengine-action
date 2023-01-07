12b79367300d2a90f5089e909dfb12db50c1a983
MysticBoy <mysticboy@live.com>
2023-01-07 19:35:53 +0800

    主机名

--- setup-tdengine.sh
+++ setup-tdengine.sh
@@ -2,5 +2,5 @@
 if [ "$TDENGINE_START" = "true" ]
 then
     sudo systemctl start taosd
-    until  curl -L -H "Authorization: Basic cm9vdDp0YW9zZGF0YQ=="   -d "SELECT SERVER_STATUS();"   127.0.0.1:6041/rest/sql; do sleep 1 &&  systemctl status taosd; done
+    until  curl -L -H "Authorization: Basic cm9vdDp0YW9zZGF0YQ=="   -d "SELECT SERVER_STATUS();"   $(hostname):6041/rest/sql; do sleep 1 &&  systemctl status taosd; done
 fi
