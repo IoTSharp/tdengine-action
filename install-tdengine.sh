72bbb5f9db7fe3b82f0d529855c82d14952cff89
MysticBoy <mysticboy@live.com>
2023-01-07 19:12:38 +0800

    修改路径

--- install-tdengine.sh
+++ install-tdengine.sh
@@ -2,9 +2,9 @@ cd /tmp
 if [ "$TDENGINE_VERSION" = "latest" ];
 then
   export TDENGINE_VERSION=$(curl -s "https://api.github.com/repos/taosdata/TDengine/releases" | jq -r '. | sort_by(.tag_name) | reverse[] | .tag_name' | sed -e 's/ver-//g' | head -n 1)
-  export TDENGINE_DOWNLOAD_PATH="https://www.taosdata.com/assets-download/3.0/"
+  export TDENGINE_DOWNLOAD_PATH="https://www.taosdata.com/assets-download/3.0"
 else
-  export TDENGINE_DOWNLOAD_PATH="https://www.taosdata.com/assets-download/3.0/"
+  export TDENGINE_DOWNLOAD_PATH="https://www.taosdata.com/assets-download/3.0"
 fi
 echo $TDENGINE_VERSION
 echo $TDENGINE_DOWNLOAD_PATH
