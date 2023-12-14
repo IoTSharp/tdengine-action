6dc08ce702095aa8076ce7e51849d0b38196dc95
MysticBoy <mysticboy@live.com>
2023-12-15 01:42:31 +0800

    Update setup-tdengine.sh

--- setup-tdengine.sh
+++ setup-tdengine.sh
@@ -8,5 +8,5 @@ then
     sudo systemctl start taos-explorer
     export TDENGINE_REST=$(hostname):6041/rest/sql
     count=0
-    until  curl -L -H "Authorization: Basic cm9vdDp0YW9zZGF0YQ=="   -d "SELECT SERVER_STATUS();"  $TDENGINE_REST ; do sleep 1 &&    ((count++)) && if [ "$count" -lt 20 ]; then exit -1 && systemctl status taosd &&  systemctl status taosadapter; done
+    until  curl -L -H "Authorization: Basic cm9vdDp0YW9zZGF0YQ=="   -d "SELECT SERVER_STATUS();"  $TDENGINE_REST ; do sleep 1 &&    ((count++)) && if [ "$count" -lt 20 ]; then exit -1 fi  && systemctl status taosd &&  systemctl status taosadapter; done
 fi
